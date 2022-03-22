4c3db5d6f421a9044b83e295928240c26761a7b7
abi <abiam@presslabs.com>
2022-03-22 14:12:44 +0000

    updated plugin `Child Theme Configurator` version 2.5.9

--- wp-content/plugins/child-theme-configurator/includes/classes/CSS.php
+++ wp-content/plugins/child-theme-configurator/includes/classes/CSS.php
@@ -4,9 +4,9 @@ if ( !defined( 'ABSPATH' ) ) exit;
 
 /*
     Class: ChildThemeConfiguratorCSS
-    Plugin URI: http://www.childthemeconfigurator.com/
+    Plugin URI: http://www.childthemeplugin.com/
     Description: Handles all CSS input, output, parsing, normalization and storage
-    Version: 2.5.8
+    Version: 2.5.9
     Author: Lilaea Media
     Author URI: http://www.lilaeamedia.com/
     Text Domain: chld_thm_cfg
@@ -172,7 +172,7 @@ class ChildThemeConfiguratorCSS {
         $this->ignoreparnt      = 0;
         $this->qpriority        = 10;
         $this->mpriority        = 10;
-        $this->version          = '2.5.8';
+        $this->version          = '2.5.9';
         
         // do not set enqueue, not being set is used to flag old versions
 
